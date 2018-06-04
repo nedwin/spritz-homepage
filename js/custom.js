73ee7709f10c22533e6cfe8adcb024368e896a06
Ned Dwyer <dwyer.ed@gmail.com>
2018-06-04 14:19:51 -0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+
+$('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                    &&
+                    location.hostname == this.hostname
+                    ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1500, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+
+                        if ($target.is("")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+
+                        }
+                        ;
+                    });
+                }
+            }
+        });
+
+var forEach = function (t, o, r) {
+    if ("[object Object]" === Object.prototype.toString.call(t))
+        for (var c in t)
+            Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+    else
+        for (var e = 0, l = t.length; l > e; e++)
+            o.call(r, t[e], e, t)
+};
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+    forEach(hamburgers, function (hamburger) {
+        hamburger.addEventListener("click", function () {
+            this.classList.toggle("is-active");
+        }, false);
+    });
+}
+$('.hamburger').click(function () {
+    if ($(this).hasClass('is-active')) {
+        $('#mySidenav').css('left', '0px');
+    }
+    else {
+        $('#mySidenav').css('left', '-255px');
+    }
+});
+
+
+
